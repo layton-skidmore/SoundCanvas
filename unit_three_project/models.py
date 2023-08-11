d255a37d168318f90d3119e5e913e302994d6583
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2023-08-11 13:06:24 -0700

    add album and review models. created corum app

--- unit_three_project/models.py
+++ unit_three_project/models.py
@@ -7,11 +7,11 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Album(models.Model):
     name = models.CharField(max_length=100)
     artist_name = models.CharField(max_length=100)
-    rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(10)]
-    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Review(models.Model):
     text = models.TextField(max_length=300)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
