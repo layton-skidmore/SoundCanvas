46eb1cc0e171fdf19bb3c3313f1275337a2e5c95
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2023-08-13 14:01:16 -0700

    pull from dev

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    album_name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+
+
+class Thread(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
