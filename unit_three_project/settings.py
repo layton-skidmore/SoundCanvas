9de428bb4d1050ec54521da051c221cf63228543
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2023-08-17 09:58:12 -0700

    changes for deplyment

--- unit_three_project/settings.py
+++ unit_three_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/index/'
 
